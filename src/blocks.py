15a8dce1bc1f021a3c748fe61193f65b0b7e783f
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-27 12:45:33 +0530

    fix::> corrected the return type

--- src/blocks.py
+++ src/blocks.py
@@ -106,7 +106,8 @@ def read_transactions():
             with open(os.path.join(mempool_dir, filename), "r") as file:
                 # locktime ka locha #
                 txn_files.append(filename[:-5])
-        print(txn_files[:1900])
+        # print(txn_files[:1900])
+        return txn_files[:1900]
         # return txn_files[:5]
         # return ["7cd041411276a4b9d0ea004e6dd149f42cb09bd02ca5dda6851b3df068749b2d", "c990d29bd10828ba40991b687362f532df79903424647dd1f9a5e2ace3edabca", "119604185a31e515e86ba0aec70559e7169600eab5adf943039b0a8b794b40df", "c3576a146165bdd8ecbfc79f18c54c8c51abd46bc0d093b01e640b6692372a93", "9fbc187e552b9e93406df86a4ebac8b67ccc0c4c321d0297edd8ffb87d4f5a45"]
     except Exception as e:
@@ -118,7 +119,7 @@ def read_transactions():
 def main():
     # Read transaction files
     transactions = read_transactions()
-    print(f"Total transactions: {len(transactions)}")
+    # print(f"Total transactions: {len(transactions)}")
 
     if not any(transactions):
         raise ValueError("No valid transactions to include in the block")
