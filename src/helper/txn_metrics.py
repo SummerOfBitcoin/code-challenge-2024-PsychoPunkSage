0eb825895604c2015c76b0ad16656455c2d88104
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-29 16:54:07 +0530

    fix::> txn_metrics documentation done

--- src/helper/txn_metrics.py
+++ src/helper/txn_metrics.py
@@ -7,6 +7,17 @@ import helper.txn_info as txinfo
 ## Txn Weight ##
 ################
 def txn_weight(txn_filename):
+    """
+    Validate a transaction's weight based on its filename.
+
+    @param txn_filename : The filename of the transaction to calculate the weight for.
+    @type  txn_filename : string
+    @return             : A list containing the transaction's weight information:
+                              - Index 0: The transaction's size in bytes.
+                              - Index 1: The transaction's weight.
+                              - Index 2: The transaction's virtual weight.
+    @rtype              : list [int, int, float]
+    """
     tx_bytes = len(txinfo.create_raw_txn_data_full(txn_filename))//2
     tx_weight = 4*(len(txinfo.create_raw_txn_data_min(txn_filename))//2) + (tx_bytes - len(txinfo.create_raw_txn_data_min(txn_filename))//2)
     tx_virtual_weight = tx_weight/4
@@ -17,6 +28,14 @@ def txn_weight(txn_filename):
 ## FEES ##
 ##########
 def fees(txn_filename):
+    """
+    Calculate the fees for a transaction based on its filename.
+
+    @param txn_filename : The filename of the transaction to calculate the fees for.
+    @type  txn_filename : string
+    @return             : The transaction's fees, calculated as the sum of input amounts minus the sum of output amounts.
+    @rtype              : int
+    """
     file_path = os.path.join('mempool', f'{txn_filename}.json') # file path
     if os.path.exists(file_path):
         # Read the JSON data from the file
